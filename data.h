5ac63de1e382fa217a45a0074648827a5d3ee1f0
aikolesnikov <artyom.kolesnikov@gmail.com>
2016-04-02 00:10:26 +0300

    Update data.h

--- data.h
+++ data.h
@@ -3,14 +3,22 @@
 
 using namespace std;
 
-const int name_size = 50;
-const int students_ammount = 20;
-const int subj_ammount = 10;
+const int name_size = 12;
+int students_ammount = 10;
+const int subj_ammount = 4;
+
+
+char NameMArr[5][10]={"Kolya","Vasya","Petya","Zhenya","Sasha"};
+char NameWArr[5][10]={"Olya","Luda","Valya","Tanya","Katya"};
+char SurnameMArr[5][20]={"Ivanov","Petrov","Sidorov","Vasechkin","Petechkin"};
+char SurnameWArr[5][20]={"Ivanova","Petrova","Sidorova","Vasechkina","Petechkina"};
+
 
 struct STUD {
 
     char name [name_size];
-    char surname [name_size]
+    char surname [name_size];
+    char sex;
     int ArrMarks [subj_ammount];
     double avMark;
 };
